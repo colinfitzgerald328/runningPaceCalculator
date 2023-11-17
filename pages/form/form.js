e0867b76dc4afd41c4f7efc3544b928e70810e3f
Colin <colinfitzgerald328@gmail.com>
2023-11-16 16:14:08 -0800

    improvmeents

--- pages/form/form.js
+++ pages/form/form.js
@@ -1,15 +1,42 @@
 import {Input} from '@nextui-org/react';
-import {Button} from "@nextui-org/react";
+import {Button, Loading} from "@nextui-org/react";
 import styles from "./styles.module.css"
 import {useState} from "react";
+import { toaster } from 'evergreen-ui';
 
 export default function RunningInputForm() {
     const [runningTime, setRunningTime] = useState('');
     const [runningPace, setRunningPace] = useState('');
     const [runningDistance, setRunningDistance] = useState('');
     const [result, setFinalResult] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    function checkhowManyFieldsFilled(runningTime, runningPace, runningDistance) {
+        let filledFields = 0;
+      
+        if (runningTime.trim() !== '') {
+          filledFields++;
+        }
+      
+        if (runningPace.trim() !== '') {
+          filledFields++;
+        }
+      
+        if (runningDistance.trim() !== '') {
+          filledFields++;
+        }
+
+        return filledFields === 2;
+      }
 
     function handleSubmit() {
+        if (!checkhowManyFieldsFilled(runningTime, runningPace, runningDistance)) {
+            toaster.danger('Please fill in two fields', {
+                duration: 2
+            });
+            return;
+        }
+        setLoading(true);
         const xhr = new XMLHttpRequest();
         xhr.open('POST', 'https://hideousbisquesystemcall.colinfitzgeral8.repl.co/convert');
         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -17,8 +44,10 @@ export default function RunningInputForm() {
             if (xhr.status === 200) {
                 const data = JSON.parse(xhr.responseText);
                 setFinalResult(data["converted_result"])
+                setLoading(false);
             } else {
                 console.error()
+                setLoading(false);
             }
         };
         xhr.send(JSON.stringify({
@@ -36,7 +65,18 @@ export default function RunningInputForm() {
                    label="Running Pace (Minutes Per Mile)"/>
             <Input css={{width: "300px", marginBottom: "20px"}}
                    onChange={(event) => setRunningDistance(event.target.value)} label="Running Distance (Miles)"/>
-            <Button onClick={handleSubmit} css={{width: "300px", marginBottom: "20px"}}>Submit</Button>
+            <Button
+                onClick={handleSubmit}
+                css={{width: "300px", marginBottom: "20px"}}
+            >
+            {
+                loading ?
+                <Loading
+                color="secondary"
+                />
+                 : 'Submit'
+            }
+            </Button>
             {result !== '' && (
                 <div>{result}</div>
             )}
